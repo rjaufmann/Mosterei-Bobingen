909613ecb6e23187f130a959baa4eeaa2b7f5f0a
JAUFMANN-PC\Robert Jaufmann <r_jaufmann@web.de>
2018-05-18 23:25:04 +0200

    Moved to AWS

--- src/app/anmeldungen/anmeldungen.component.ts
+++ src/app/anmeldungen/anmeldungen.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import * as data from 'resources/anmeldungen.json';
+import * as data from 'resources/anmeldungen-orig.json';
 import {Abfuellung_Enum, Mitglied_Enum, SaftArt_Enum, Anmeldung} from "./anmeldung";
+import {environment} from "../../environments/environment";
 
 
 @Component({
@@ -22,36 +23,53 @@ export class AnmeldungenComponent implements OnInit {
 }
 
 interface AnmeldungJSON {
-  Mitglied : string;
-  Beginn : string;
-  Vollstaendiger_Name : string;
-  Telefonnummer : string;
-  Saftart : string;
-  Abfuellung : string;
-  Bemerkung : string;
-  Mostmenge : string;
+  id : number;
+  beginn : string;
+  finish : string;
+  userid : number;
+  statusMessage : string;
+  mitglied : string;
+  fullName : string;
+  email : string;
+  phone : string;
+  mobil : string;
+  saftart : string;
+  abfuellung : string;
+  bemerkung : string;
+  angemeldeteMenge : string;
 
 }
 
 function decodeAnmeldung(json: AnmeldungJSON): Anmeldung {
-  return {
-    mitglied : Mitglied_Enum.J,
-    beginn : json.Beginn ? new Date(json.Beginn) : null,
-    vollstaendiger_Name : json.Vollstaendiger_Name,
-    telefonnummer : json.Telefonnummer,
-    saftart : SaftArt_Enum.SM,
-    abfuellung : Abfuellung_Enum.BAGINBOX,
-    bemerkung : json.Bemerkung,
-    mostmenge : json.Mostmenge
-  };
-}
-
-function getAnmeldungen(): Promise<Anmeldung[]> {
-  return loadData().then(data => {
-    return data.map(decodeAnmeldung);
+  let anmeldung = Object.create(Anmeldung.prototype);
+  return Object.assign(anmeldung, json, {
+    beginn: new Date(json.beginn),
+    finish: new Date(json.finish)
   });
 }
 
-function loadData(): Promise<AnmeldungJSON[]> {
-  return (<any>data);
+function getAnmeldungen() {
+  if (!environment.local) {
+
+      return ajax.get<UserJSON[]>('/users').then(data => {
+        return data.data.map(decodeUser);
+      });
+    }
+  }
+  if (environment.local) {
+    let anmeldungJSONArray = loadDataFromLocal();
+    console.log(anmeldungJSONArray);
+/*
+    return loadDataFromLocal().then(data => {
+      return data.map(decodeAnmeldung);
+    });
+*/
+  }
+  else
 }
+
+function loadDataFromLocal(): Promise<AnmeldungJSON[]> {
+  return JSON.parse(data, Anmeldung.reviver);
+}
+
+
